ibmruntimes/openj9-openjdk-jdk17

8234147: Avoid looking up standard charsets in core libraries Reviewed-by: alanb

src/java.base/share/classes/java/util/jar/Manifest.java
@@ -33,9 +33,8 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import sun.nio.cs.UTF_8;
 import sun.security.util.SecurityProperties;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * The Manifest class is used to maintain Manifest entry names and their
@@ -237,7 +236,7 @@ public class Manifest implements Cloneable {
      */
     static void println72(OutputStream out, String line) throws IOException {
         if (!line.isEmpty()) {
-            byte[] lineBytes = line.getBytes(UTF_8);
+            byte[] lineBytes = line.getBytes(UTF_8.INSTANCE);
             int length = lineBytes.length;
             // first line can hold one byte more than subsequent lines which
             // start with a continuation line break space
@@ -337,7 +336,7 @@ public class Manifest implements Cloneable {
                     lastline = buf;
                     continue;
                 }
-                name = new String(buf, 0, buf.length, UTF_8);
+                name = new String(buf, 0, buf.length, UTF_8.INSTANCE);
                 lastline = null;
             }
             Attributes attr = getAttributes(name);
@@ -362,11 +361,7 @@ public class Manifest implements Cloneable {
         if (toLower(lbuf[0]) == 'n' && toLower(lbuf[1]) == 'a' &&
             toLower(lbuf[2]) == 'm' && toLower(lbuf[3]) == 'e' &&
             lbuf[4] == ':' && lbuf[5] == ' ') {
-            try {
-                return new String(lbuf, 6, len - 6, UTF_8);
-            }
-            catch (Exception e) {
-            }
+            return new String(lbuf, 6, len - 6, UTF_8.INSTANCE);
         }
         return null;
     }

bell-sw/jdk11_jetbrains

src/java.base/share/classes/java/util/jar/Manifest.java
@@ -33,6 +33,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.HashMap;
 
+import sun.nio.cs.UTF_8;
+
 /**
  * The Manifest class is used to maintain Manifest entry names and their
  * associated Attributes. There are main Manifest Attributes as well as
@@ -187,7 +189,7 @@ public class Manifest implements Cloneable {
             StringBuffer buffer = new StringBuffer("Name: ");
             String value = e.getKey();
             if (value != null) {
-                byte[] vb = value.getBytes("UTF8");
+                byte[] vb = value.getBytes(UTF_8.INSTANCE);
                 value = new String(vb, 0, 0, vb.length);
             }
             buffer.append(value);
@@ -272,7 +274,7 @@ public class Manifest implements Cloneable {
                     lastline = buf;
                     continue;
                 }
-                name = new String(buf, 0, buf.length, "UTF8");
+                name = new String(buf, 0, buf.length, UTF_8.INSTANCE);
                 lastline = null;
             }
             Attributes attr = getAttributes(name);
@@ -297,11 +299,7 @@ public class Manifest implements Cloneable {
         if (toLower(lbuf[0]) == 'n' && toLower(lbuf[1]) == 'a' &&
             toLower(lbuf[2]) == 'm' && toLower(lbuf[3]) == 'e' &&
             lbuf[4] == ':' && lbuf[5] == ' ') {
-            try {
-                return new String(lbuf, 6, len - 6, "UTF8");
-            }
-            catch (Exception e) {
-            }
+            return new String(lbuf, 6, len - 6, UTF_8.INSTANCE);
         }
         return null;
     }