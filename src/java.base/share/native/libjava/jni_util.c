JetBrains/JetBrainsRuntime

JBR-7600 Provide ability to add messages to fatal error log
Use JNU_LOG_EVENT(env, msg, ...) to save a message in the internal
JVM ring buffer that gets printed out to the Events section
of the fatal error log if JVM crashed.

src/java.base/share/native/libjava/jni_util.c
@@ -1320,3 +1320,40 @@ JNU_Fatal(JNIEnv *env, const char *file, int line, const char *msg)
         (*env)->ThrowNew(env, cls, real_msg);
     }
 }
+
+JNIEXPORT void JNICALL
+JNU_LogEvent(JNIEnv *env, const char *file, int line, const char *fmt, ...)
+{
+    jclass cls = (*env)->FindClass(env, "java/lang/Exception$JB$$Event");
+    if (cls == 0) return;
+
+    va_list args;
+
+    va_start(args, fmt);
+    int len = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+    if (len < 0) return;
+
+    int suffix_len = (int) strlen(file) + 64;
+    len += suffix_len;
+    char * real_msg = malloc(len);
+    if (real_msg == NULL) return;
+
+    va_start(args, fmt);
+    vsnprintf(real_msg, len, fmt, args);
+    va_end(args);
+
+    char * suffix = malloc(suffix_len);
+    if (suffix == NULL) {
+        free(real_msg);
+        return;
+    }
+
+    snprintf(suffix, suffix_len, " (%s:%d)", file, line);
+    strncat(real_msg, suffix, len);
+    free(suffix);
+
+    // Throwing an exception by this name will result in Events::log() call
+    (*env)->ThrowNew(env, cls, real_msg);
+    free(real_msg);
+}

bell-sw/jdk11_jetbrains

src/java.base/share/native/libjava/jni_util.c
@@ -1320,3 +1320,40 @@ JNU_Fatal(JNIEnv *env, const char *file, int line, const char *msg)
         (*env)->ThrowNew(env, cls, real_msg);
     }
 }
+
+JNIEXPORT void JNICALL
+JNU_LogEvent(JNIEnv *env, const char *file, int line, const char *fmt, ...)
+{
+    jclass cls = (*env)->FindClass(env, "java/lang/Exception$JB$$Event");
+    if (cls == 0) return;
+
+    va_list args;
+
+    va_start(args, fmt);
+    int len = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+    if (len < 0) return;
+
+    int suffix_len = (int) strlen(file) + 64;
+    len += suffix_len;
+    char * real_msg = malloc(len);
+    if (real_msg == NULL) return;
+
+    va_start(args, fmt);
+    vsnprintf(real_msg, len, fmt, args);
+    va_end(args);
+
+    char * suffix = malloc(suffix_len);
+    if (suffix == NULL) {
+        free(real_msg);
+        return;
+    }
+
+    snprintf(suffix, suffix_len, " (%s:%d)", file, line);
+    strncat(real_msg, suffix, suffix_len);
+    free(suffix);
+
+    // Throwing an exception by this name will result in Events::log() call
+    (*env)->ThrowNew(env, cls, real_msg);
+    free(real_msg);
+}