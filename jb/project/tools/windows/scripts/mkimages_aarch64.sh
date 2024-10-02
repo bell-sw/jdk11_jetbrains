JetBrains/JetBrainsRuntime

JBR-6246 add default CDS archives into jbrsdk distributions

jb/project/tools/windows/scripts/mkimages_aarch64.sh
@@ -73,6 +73,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/lib/server/*.jsa $__root_dir/lib/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done

jb/project/tools/windows/scripts/mkimages_x64.sh
@@ -66,6 +66,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/lib/server/*.jsa $__root_dir/lib/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done

jb/project/tools/windows/scripts/mkimages_x86.sh
@@ -62,6 +62,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/lib/server/*.jsa $__root_dir/lib/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done

bell-sw/jdk11_jetbrains

jb/project/tools/windows/scripts/mkimages_aarch64.sh
@@ -73,6 +73,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/bin/server/*.jsa $__root_dir/bin/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done

jb/project/tools/windows/scripts/mkimages_x64.sh
@@ -66,6 +66,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/bin/server/*.jsa $__root_dir/bin/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done

jb/project/tools/windows/scripts/mkimages_x86.sh
@@ -62,6 +62,7 @@ function create_image_bundle {
     sed 's/JBR/JBRSDK/g' $__root_dir/release > release
     mv release $__root_dir/release
     cp $IMAGES_DIR/jdk/lib/src.zip $__root_dir/lib
+    cp $IMAGES_DIR/jdk/bin/server/*.jsa $__root_dir/bin/server
     for dir in $(ls -d $IMAGES_DIR/jdk/*); do
       rsync -amv --include="*/" --include="*.pdb" --exclude="*" $dir $__root_dir
     done