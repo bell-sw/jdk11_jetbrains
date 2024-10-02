JetBrains/JetBrainsRuntime

JBR-5965 Wayland: implement SplashScreen

src/java.base/unix/native/libjli/java_md.c
@@ -154,6 +154,14 @@
 /* Store the name of the executable once computed */
 static char *execname = NULL;
 
+typedef enum awt_toolkit {
+    TK_UNKNOWN = 0,
+    TK_X11 = 1,
+    TK_WAYLAND = 2
+} awt_toolkit;
+
+static awt_toolkit _awt_toolkit = TK_UNKNOWN;
+
 /*
  * execname accessor from other parts of platform dependent logic
  */
@@ -646,7 +654,6 @@ SetExecname(char **argv)
 }
 
 /* --- Splash Screen shared library support --- */
-static const char* SPLASHSCREEN_SO = JNI_LIB_NAME("splashscreen");
 static void* hSplashLib = NULL;
 
 void* SplashProcAddress(const char* name) {
@@ -659,8 +666,15 @@ void* SplashProcAddress(const char* name) {
             JLI_ReportErrorMessage(JRE_ERROR1);
             return NULL;
         }
+
+#if defined(__linux__)
+        const char* splash_screen_so = _awt_toolkit == TK_WAYLAND ?
+                                      JNI_LIB_NAME("wlsplashscreen") : JNI_LIB_NAME("splashscreen");
+#else
+        const char* splash_screen_so = JNI_LIB_NAME("splashscreen");
+#endif
         ret = JLI_Snprintf(splashPath, sizeof(splashPath), "%s/lib/%s",
-                     jrePath, SPLASHSCREEN_SO);
+                     jrePath, splash_screen_so);
 
         if (ret >= (int) sizeof(splashPath)) {
             JLI_ReportErrorMessage(JRE_ERROR11);
@@ -747,11 +761,34 @@ CallJavaMainInNewThread(jlong stack_size, void* args) {
 /* Coarse estimation of number of digits assuming the worst case is a 64-bit pid. */
 #define MAX_PID_STR_SZ   20
 
+static char*
+getToolkitNameByEnv() {
+    if (_awt_toolkit == TK_UNKNOWN) {
+        char *xdg_session_type = getenv("XDG_SESSION_TYPE");
+        if (xdg_session_type != NULL && strcmp(xdg_session_type, "wayland") == 0) {
+            _awt_toolkit = TK_WAYLAND;
+        } else if (xdg_session_type != NULL && strcmp(xdg_session_type, "x11") == 0) {
+            _awt_toolkit = TK_X11;
+        } else if (getenv("DISPLAY") != NULL) {
+            _awt_toolkit = TK_X11;
+        } else if (getenv("WAYLAND_DISPLAY") != NULL) {
+            _awt_toolkit = TK_WAYLAND;
+        }
+    }
+    return _awt_toolkit == TK_WAYLAND ? "WLToolkit" : "XToolkit";
+}
+
 int
 JVMInit(InvocationFunctions* ifn, jlong threadStackSize,
         int argc, char **argv,
         int mode, char *what, int ret)
 {
+    char *toolkit_name = getToolkitNameByEnv();
+    size_t toolkit_name_size = JLI_StrLen("-Dawt.toolkit.name=") + JLI_StrLen(toolkit_name) + 1;
+    char *toolkit_option = (char *)JLI_MemAlloc(toolkit_name_size);
+    snprintf(toolkit_option, toolkit_name_size, "-Dawt.toolkit.name=%s", toolkit_name);
+    AddOption(toolkit_option, NULL);
+
     ShowSplashScreen();
     return ContinueInNewThread(ifn, threadStackSize, argc, argv, mode, what, ret);
 }
@@ -768,11 +805,16 @@ RegisterThread()
     // stubbed out for windows and *nixes.
 }
 
-/*
- * on unix, we return a false to indicate this option is not applicable
- */
 jboolean
 ProcessPlatformOption(const char *arg)
 {
+    if (JLI_StrCCmp(arg, "-Dawt.toolkit.name=WLToolkit") == 0) {
+        _awt_toolkit = TK_WAYLAND;
+        return JNI_TRUE;
+    } else if (JLI_StrCCmp(arg, "-Dawt.toolkit.name=XToolkit") == 0) {
+        _awt_toolkit = TK_X11;
+        return JNI_TRUE;
+    }
+
     return JNI_FALSE;
 }

bell-sw/jdk11_jetbrains

src/java.base/unix/native/libjli/java_md.c
@@ -138,6 +138,17 @@
 /* Store the name of the executable once computed */
 static char *execname = NULL;
 
+typedef enum awt_toolkit {
+    TK_UNKNOWN = 0,
+    TK_X11 = 1,
+    TK_WAYLAND = 2
+} awt_toolkit;
+
+// TODO when wayland support will be fully implemented change to TK_UNKNOWN
+// currently wayland support is not Production-Ready ready so default awt toolkit is X11
+// wayland could be chosen manually via passing -Dawt.toolkit.name=WLToolkit argument
+static awt_toolkit _awt_toolkit = TK_X11;
+
 /*
  * execname accessor from other parts of platform dependent logic
  */
@@ -629,7 +640,6 @@ SetExecname(char **argv)
 }
 
 /* --- Splash Screen shared library support --- */
-static const char* SPLASHSCREEN_SO = JNI_LIB_NAME("splashscreen");
 static void* hSplashLib = NULL;
 
 void* SplashProcAddress(const char* name) {
@@ -642,8 +652,15 @@ void* SplashProcAddress(const char* name) {
             JLI_ReportErrorMessage(JRE_ERROR1);
             return NULL;
         }
+
+#if defined(__linux__)
+        const char* splash_screen_so = _awt_toolkit == TK_WAYLAND ?
+                                      JNI_LIB_NAME("wlsplashscreen") : JNI_LIB_NAME("splashscreen");
+#else
+        const char* splash_screen_so = JNI_LIB_NAME("splashscreen");
+#endif
         ret = JLI_Snprintf(splashPath, sizeof(splashPath), "%s/lib/%s",
-                     jrePath, SPLASHSCREEN_SO);
+                     jrePath, splash_screen_so);
 
         if (ret >= (int) sizeof(splashPath)) {
             JLI_ReportErrorMessage(JRE_ERROR11);
@@ -730,11 +747,34 @@ CallJavaMainInNewThread(jlong stack_size, void* args) {
 /* Coarse estimation of number of digits assuming the worst case is a 64-bit pid. */
 #define MAX_PID_STR_SZ   20
 
+static char*
+getToolkitNameByEnv() {
+    if (_awt_toolkit == TK_UNKNOWN) {
+        char *xdg_session_type = getenv("XDG_SESSION_TYPE");
+        if (xdg_session_type != NULL && strcmp(xdg_session_type, "wayland") == 0) {
+            _awt_toolkit = TK_WAYLAND;
+        } else if (xdg_session_type != NULL && strcmp(xdg_session_type, "x11") == 0) {
+            _awt_toolkit = TK_X11;
+        } else if (getenv("DISPLAY") != NULL) {
+            _awt_toolkit = TK_X11;
+        } else if (getenv("WAYLAND_DISPLAY") != NULL) {
+            _awt_toolkit = TK_WAYLAND;
+        }
+    }
+    return _awt_toolkit == TK_WAYLAND ? "WLToolkit" : "XToolkit";
+}
+
 int
 JVMInit(InvocationFunctions* ifn, jlong threadStackSize,
         int argc, char **argv,
         int mode, char *what, int ret)
 {
+    char *toolkit_name = getToolkitNameByEnv();
+    size_t toolkit_name_size = JLI_StrLen("-Dawt.toolkit.name=") + JLI_StrLen(toolkit_name) + 1;
+    char *toolkit_option = (char *)JLI_MemAlloc(toolkit_name_size);
+    snprintf(toolkit_option, toolkit_name_size, "-Dawt.toolkit.name=%s", toolkit_name);
+    AddOption(toolkit_option, NULL);
+
     ShowSplashScreen();
     return ContinueInNewThread(ifn, threadStackSize, argc, argv, mode, what, ret);
 }
@@ -751,11 +791,16 @@ RegisterThread()
     // stubbed out for windows and *nixes.
 }
 
-/*
- * on unix, we return a false to indicate this option is not applicable
- */
 jboolean
 ProcessPlatformOption(const char *arg)
 {
+    if (JLI_StrCCmp(arg, "-Dawt.toolkit.name=WLToolkit") == 0) {
+        _awt_toolkit = TK_WAYLAND;
+        return JNI_TRUE;
+    } else if (JLI_StrCCmp(arg, "-Dawt.toolkit.name=XToolkit") == 0) {
+        _awt_toolkit = TK_X11;
+        return JNI_TRUE;
+    }
+
     return JNI_FALSE;
 }